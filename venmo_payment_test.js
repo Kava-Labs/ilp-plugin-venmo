caeed9772d293fce9336cb879d5ee7f86692816d
Kevin Davis <kjydavis3@gmail.com>
2018-02-26 15:22:30 -0500

    made demo headless

--- venmo_payment_test.js
+++ venmo_payment_test.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const VenmoAPI = require('./venmo-api.js')
+// Define these in your PATH
 const venmo_email = process.env.VENMO_EMAIL
 const venmo_pass = process.env.VENMO_PASS
 
@@ -8,16 +9,17 @@ const venmo_pass = process.env.VENMO_PASS
 async function sendMoney(
         api_email, api_pass, destinationUsername, amount) {
     let venmo = await new VenmoAPI(api_email, api_pass)
-    await venmo.connect(false)
+    await venmo.connect()
     let balance = await venmo.getBalance()
     if (balance < amount) {
         console.log("Insufficient funds.")
         return
     }
     await venmo.sendMoney(destinationUsername, amount)
+    await venmo.disconnect()
 }
 
 
-sendMoney(venmo_email, venmo_pass, "ruaridh", 0.12)
+sendMoney(venmo_email, venmo_pass, "ruaridh", 0.10)
 
 
